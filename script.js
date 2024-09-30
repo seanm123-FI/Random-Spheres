fb8021c27443ad7aac6f722c5da96522e57f3616
sean murphy <sean.murphy@fiserv.com>
2024-09-30 15:47:51 +0100

    dbl click

--- script.js
+++ script.js
@@ -1,66 +1,84 @@
- document.addEventListener("DOMContentLoaded", function() {    //good practice to use eventlistener as it ensure that all content is loaded before js script is run
-    var body = document.querySelector("body");  // gets body element of HTML 
+document.addEventListener("DOMContentLoaded", function() {
+  // Good practice to use event listener to ensure that all content is loaded before JS script runs
+  const body = document.querySelector("body");  // gets body element of HTML 
 
-    var numOfSpheres = Math.floor(Math.random() * 10);  //Math random creates a decimal between 0 and 1, ie 0.4785. * 10 to give 4.785 and then flooring it to 4
-    var colours = ['green', 'yellow', 'blue', 'pink', 'yellow', 'purple', 'red', 'orange'];   //array of potential sphere colours
+  const numOfSpheres = Math.floor(Math.random() * 10);  // Math random creates a decimal between 0 and 1, e.g., 0.4785. * 10 to give 4.785 and then flooring it to 4
+  const colours = ['green', 'yellow', 'blue', 'pink', 'yellow', 'purple', 'red', 'orange'];   // array of potential sphere colours
 
-    for (var i = 0; i < numOfSpheres; i++) {
+  for (let i = 0; i < numOfSpheres; i++) {
 
-        var createSphere = document.createElement("div");  //creates new div element in HTML
-        createSphere.className = "sphere";                //assigning the defined sphere CSS element to 'createSphere'
+      const createSphere = document.createElement("div");  // creates new div element in HTML
+      createSphere.className = "sphere";  // assigning the defined sphere CSS element to 'createSphere'
 
-        var x = Math.floor(Math.random() * (window.innerWidth - 100));       //gives random xpos by getting number between 0 and 1 ie. 0.2324 and multiplying by screen width ie. 1000 = 232.4 and flooring it to give sphere xpos of 232
-        var y = Math.floor(Math.random() * (window.innerHeight - 100));       //gives random ypos by getting number between 0 and 1 ie. 0.5712 and multiplying by screen height ie. 1000 = 571.2 and flooring it to give sphere xpos of 571
-        createSphere.style.left = x + "px";                             //starts at left side of screen and places sphere onto screen at xpos by whatever the x amount was calculated at
-        createSphere.style.top = y + "px"; // Changed to 'top' to make the spheres draggable vertically
+      const x = Math.floor(Math.random() * (window.innerWidth - 100));  // gives random xpos by getting a number between 0 and 1, e.g., 0.2324, and multiplying by screen width, e.g., 1000, to give 232.4 and flooring it to give sphere xpos of 232
+      const y = Math.floor(Math.random() * (window.innerHeight - 100));  // gives random ypos by getting a number between 0 and 1, e.g., 0.5712, and multiplying by screen height, e.g., 1000, to give 571.2 and flooring it to give sphere ypos of 571
+      createSphere.style.left = x + "px";  // starts at the left side of the screen and places sphere onto screen at xpos by whatever the "x" amount was calculated at
+      createSphere.style.top = y + "px";  // starts at the top of the screen and places sphere onto screen at ypos by whatever the "y" amount was calculated at
 
-        var sphereColour = colours[Math.floor(Math.random() * colours.length)];      //gets number between 0 and 1 ie. 0.3489 * 8 = 2.7912, floored to 2 so blue is selected and assigned to sphereColour
-        createSphere.style.background = `radial-gradient(circle at 50% 50%, ${sphereColour}, black)`; //assigning radial gradient background style to sphere to give more spherical perception, sphere colour is the random selection in prev line and black is the second colour of the gradient
-        createSphere.style.boxShadow = ` 0 0 3px 1.5px ${sphereColour}`;       
-   
-        body.appendChild(createSphere);  //adds sphere to HTML doc as a child of the body element which makes it visible on page
+      const sphereColour = colours[Math.floor(Math.random() * colours.length)];  // gets a number between 0 and 1, e.g., 0.3489 * 8 = 2.7912, floored to 2, so blue is selected and assigned to "sphereColour"
+      createSphere.style.background = `radial-gradient(circle at 50% 50%, ${sphereColour}, black)`; // assigning radial gradient background style to sphere to give more spherical perception, sphere colour is the random selection in the previous line, and black is the second colour of the gradient
+      createSphere.style.boxShadow = `0 0 3px 1.5px ${sphereColour}`;       
 
-        createSphere.addEventListener('mousedown', function(draggable){
-            var initialX = draggable.clientX;
-            var initialY = draggable.clientY;
-            var sphere = this;
+      body.appendChild(createSphere);  // adds sphere to HTML doc as a child of the body element which makes it visible on the page
 
-            function onMouseMove(draggable){
-                var diffX = draggable.clientX - initialX;
-                var diffY = draggable.clientY - initialY;
+      // Draggable functionality
+      createSphere.addEventListener('mousedown', function(event) {
+          let initialX = event.clientX;
+          let initialY = event.clientY;
+          const sphere = this;
 
-                var sphereX = parseInt(sphere.style.left || 0) + diffX;
-                var sphereY = parseInt(sphere.style.top || 0) + diffY;
+          function mouseMove(event) {
+              const diffX = event.clientX - initialX;
+              const diffY = event.clientY - initialY;
 
-                sphere.style.left = sphereX + 'px';
-                sphere.style.top = sphereY + 'px';
- 
-                initialX = draggable.clientX;
-                initialY = draggable.clientY;
+              const sphereX = parseInt(sphere.style.left || 0) + diffX;
+              const sphereY = parseInt(sphere.style.top || 0) + diffY;
 
-            }
+              sphere.style.left = sphereX + 'px';
+              sphere.style.top = sphereY + 'px';
 
-        function onMouseUp() {
-          document.removeEventListener('mousemove', onMouseMove);
-          document.removeEventListener('mouseup', onMouseUp);
-        }
-  
-        document.addEventListener('mousemove', onMouseMove);
-        document.addEventListener('mouseup', onMouseUp);
-        
-        });
+              initialX = event.clientX;
+              initialY = event.clientY;
+          }
 
-        createSphere.addEventListener('dblclick', function(){
+          function mouseUp() {
+              document.removeEventListener('mousemove', mouseMove);
+              document.removeEventListener('mouseup', mouseUp);
+          }
 
-        }
-    }
+          document.addEventListener('mousemove', mouseMove);
+          document.addEventListener('mouseup', mouseUp);
+      });
 
- });
+      // Double-click to bounce functionality
+      createSphere.addEventListener('dblclick', function() {
+          const speedX = 3;
+          const speedY = 3;
+          const sphere = this;
 
- //use rgb colours
- //move balls with mouse, draggable
- //when ball is double clicked, will bounce when ball is clicked 
+          function moveBall() {
+              let posX = parseInt(sphere.style.left) || 0;
+              let posY = parseInt(sphere.style.top) || 0;
+              const sphereWidth = sphere.clientWidth || 0;
+              const sphereHeight = sphere.clientHeight || 0;
 
-//  http , SMTP, POP3, IMAP, GOPHER, NEWS
+              if (posX <= 0 || posX >= window.innerWidth - sphereWidth) {
+                  speedX *= -1;
+              }
+              if (posY <= 0 || posY >= window.innerHeight - sphereHeight) {
+                  speedY *= -1;
+              }
 
-            
+              posX += speedX;
+              posY += speedY;
+
+              sphere.style.left = posX + 'px';
+              sphere.style.top = posY + 'px';
+
+              requestAnimationFrame(moveBall);
+          }
+
+          moveBall();
+      });
+  }
+});
